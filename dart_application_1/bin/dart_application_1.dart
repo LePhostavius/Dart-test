f0c32c9d5beb530db2367671c10b1027930a5ae6
Louis Falce <louis.falce@hotmail.fr>
2022-05-03 22:58:31 +0200

    Xavier je suis sur que tu tomberas la dessus fouineur

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,5 +1,8 @@
 import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
 
+
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
+  var a = "Xavier je suis sur que tu tomberas la dessus un jour";
+  print(a);
 }
